22c7b9e52ca285b051c26b684ff562e370006e48
Michelle <mtham101@gmail.com>
2016-05-17 11:04:03 -0700

    Added simple animation

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ var app = express()
 
 app.use("/", express.static(__dirname))
 
-var port = process.env.PORT || 8080
+var port = process.env.PORT || 3000
 
-app.listen(port, console.log("Magic happens on 8080"))
+app.listen(port, console.log("Magic happens on 3000"))
 
 
 
